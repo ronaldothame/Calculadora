af2fbef7e005a084f5384aff9ba0f8c58aae8de4
rthame <ronaldothame1@gmail.com>
2024-03-26 11:36:18 -0300

    created some tests for each of the basic math operations

--- src/main/java/org/example/Divisao.java
+++ src/main/java/org/example/Divisao.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Divisao {
-    public static double dividr (double num1, double num2) {
+    public static double dividir (double num1, double num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("Não é possível dividir por zero");
         }
