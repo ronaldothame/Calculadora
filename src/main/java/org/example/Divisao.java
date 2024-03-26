0a28c2217d1f084950e89d2dc5e9b8a54d2fe7e9
rthame <ronaldothame1@gmail.com>
2024-03-26 11:47:52 -0300

    refactored 'dividir' method

--- src/main/java/org/example/Divisao.java
+++ src/main/java/org/example/Divisao.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class Divisao {
-    public static double dividir (double num1, double num2) {
+    public static double dividir(double num1, double num2) {
         if (num2 == 0) {
-            throw new IllegalArgumentException("Não é possível dividir por zero");
+            return Double.NaN;
         }
         return num1 / num2;
     }
