a1fbeafbed97775b5b942a6c42bc86ca54d749ff
rthame <ronaldothame1@gmail.com>
2024-03-26 11:26:29 -0300

    added Divisao class

---
+++ src/main/java/org/example/Divisao.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Divisao {
+    public static double dividr (double num1, double num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Não é possível dividir por zero");
+        }
+        return num1 / num2;
+    }
+}
--- src/main/java/org/example/Teste.java
+++ src/main/java/org/example/Teste.java
@@ -2,6 +2,5 @@ package org.example;
 
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
